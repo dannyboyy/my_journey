8fd1da13a139fbb71041b0b064bae3e72bf6de97
Danny Wu <wushuo1988@hotmail.com>
2014-04-21 22:32:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyJourney::Application.config.secret_key_base = '0e8bfa86cacc9cb524f9c979f3e63277f6384be0b33c75c220b858a1cb805df37adc4ee9ca02d286b7f6537f9e4282aa5c3238b12130dbba06a9206e9f7949cc'
